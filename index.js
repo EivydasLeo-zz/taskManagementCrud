0ce2a8cccdade7ee4b6286bb1ee92dfd6e4202a0
Your Name <you@example.com>
2021-08-11 20:51:40 +0300

    connected to mongooDB

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const PORT = 4000;
+
+// prisijungimas prie duomenu bazes
+mongoose
+  .connect(process.env.MONGO_CONNECT_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Conneced to MongooDB');
+  })
+  .catch((err) => console.error(err.message));
 
 app.get('/', (req, res) => {
   res.status(200).json(`Server is working at PORT ${PORT}`);
