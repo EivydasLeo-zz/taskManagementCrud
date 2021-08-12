8cb00a790d939f136788ce566922fdd6d7542e01
Your Name <you@example.com>
2021-08-12 13:58:26 +0300

    react-router-dom i, added Link navbar.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './Navbar.css';
 import nfqLogo from '../Navbar/Nfq.png';
-
+import { Link } from 'react-router-dom';
 class Navbar extends Component {
   state = {};
   render() {
     return (
       <div className="navbarContainer">
         <div className="navbarImg">
-          <img src={nfqLogo} alt="nfq-logo" />
+          <Link to="/">
+            <img src={nfqLogo} alt="nfq-logo" />
+          </Link>
         </div>
       </div>
     );
