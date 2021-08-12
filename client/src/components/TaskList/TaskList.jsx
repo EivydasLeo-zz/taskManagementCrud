5f45e2c9fbb735c180c476eabba5d4d4c40bd01d
Your Name <you@example.com>
2021-08-12 13:59:13 +0300

    TaskList table created, css styling

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -5,9 +5,32 @@ class TaskList extends Component {
   state = {};
   render() {
     return (
-      <div className="taskContainer">
-        <div className="taskTitle">
-          <h1>Task management system</h1>
+      <div className="taskListContainer">
+        <div className="taskList-Body">
+          <div className="taskList-Table">
+            <h3>Task management system</h3>
+            <button>Add Task</button>
+            <table>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>More Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Meeting</td>
+                  <td>Meeting with team</td>
+                  <td>Friday</td>
+                  <td>Incoming</td>
+                  <button>View task details</button>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
