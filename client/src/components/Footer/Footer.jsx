7e6766117c89b82f1232078fc7c7d65fd6f06050
Your Name <you@example.com>
2021-08-14 21:33:26 +0300

    Added footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentYear: new Date().getFullYear(),
+    };
+  }
+  render() {
+    return (
+      <footer className="footer-Container">
+        <p className="footer__text">
+          Project created by Eivydas Leonavicius. All rights reserved &copy; {this.state.currentYear}
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
