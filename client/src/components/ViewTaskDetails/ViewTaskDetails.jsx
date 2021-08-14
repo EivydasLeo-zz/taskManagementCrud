caf3fe99f92468af98cb0020137e74ba3ddbf3fa
Your Name <you@example.com>
2021-08-14 10:48:15 +0300

    fixed getSingleTask, displays object in fe

--- client/src/components/ViewTaskDetails/ViewTaskDetails.jsx
+++ client/src/components/ViewTaskDetails/ViewTaskDetails.jsx
@@ -9,33 +9,25 @@ class ViewTaskDetails extends Component {
     };
   }
 
-  async getTask() {
+  async componentDidMount() {
     const currentTaskId = this.props.match.params.id;
     const task = await getSingleTask(currentTaskId);
-    console.log(task);
-    console.log(this.props);
     this.setState({ taskDetailsData: task });
-  }
-
-  componentDidMount() {
-    this.getTask();
   }
 
   render() {
     return (
       <div className="viewTaskDetail-Container">
-        {this.state.taskDetailsData.map((obj) => (
-          <div className="viewTaskDetail-cardBody" key={obj._id}>
-            <h6 className="cardBody-Title">Title: {obj.title} </h6>
-            <h6 className="cardBody-Description">Description:{obj.description} </h6>
-            <h6 className="cardBody-Category">Category: {obj.category} </h6>
-            <h6 className="cardBody-Status">Status: {obj.status} </h6>
-            <h6 className="cardBody-CreationDate">Creation Date: </h6>
-            <h6 className="cardBody-UpdateDate">Update Date: </h6>
-            <button className="cardBody-EditBtn">Edit</button>
-            <button className="cardBody-DeleteBtn">Delete</button>
-          </div>
-        ))}
+        <div className="viewTaskDetail-cardBody" key={this.state.taskDetailsData._id}>
+          <h6 className="cardBody-Title">Title: {this.state.taskDetailsData.title} </h6>
+          <h6 className="cardBody-Description">Description:{this.state.taskDetailsData.description} </h6>
+          <h6 className="cardBody-Category">Category: {this.state.taskDetailsData.category} </h6>
+          <h6 className="cardBody-Status">Status: {this.state.taskDetailsData.status} </h6>
+          <h6 className="cardBody-CreationDate">Creation Date: </h6>
+          <h6 className="cardBody-UpdateDate">Update Date: </h6>
+          <button className="cardBody-EditBtn">Edit</button>
+          <button className="cardBody-DeleteBtn">Delete</button>
+        </div>
       </div>
     );
   }
